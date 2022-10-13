3050f0e405b576cef5b6b24f1b369f11d1c748c1
Jeff <scubajeffrey@criptext.com>
2022-10-13 12:12:46 +0800

    removed obsolete functions and styles

--- js/main.js
+++ js/main.js
@@ -1,179 +1,17 @@
 ;(function () {
-	
+
 	'use strict';
 
-
-
-	// iPad and iPod detection	
+	// iPad and iPod detection
 	var isiPad = function(){
 		return (navigator.platform.indexOf("iPad") != -1);
 	};
 
 	var isiPhone = function(){
 	    return (
-			(navigator.platform.indexOf("<i></i>Phone") != -1) || 
+			(navigator.platform.indexOf("<i></i>Phone") != -1) ||
 			(navigator.platform.indexOf("iPod") != -1)
 	    );
-	};
-
-	
-	
-
-	// Click outside of offcanvass
-	var mobileMenuOutsideClick = function() {
-
-		$(document).click(function (e) {
-	    var container = $("#fh5co-offcanvas, .js-fh5co-close-offcanvas");
-	    if (!container.is(e.target) && container.has(e.target).length === 0) {
-
-	    	if ( $('#fh5co-offcanvas').hasClass('animated fadeInLeft') ) {
-
-    			$('#fh5co-offcanvas').addClass('animated fadeOutLeft');
-				setTimeout(function(){
-					$('#fh5co-offcanvas').css('display', 'none');	
-					$('#fh5co-offcanvas').removeClass('animated fadeOutLeft fadeInLeft');
-				}, 1000);
-				$('.js-fh5co-nav-toggle').removeClass('active');
-				
-	    	}
-	    
-	    	
-	    }
-		});
-
-		$('body').on('click', '.js-fh5co-close-offcanvas', function(event){
-		
-
-	  		$('#fh5co-offcanvas').addClass('animated fadeOutLeft');
-			setTimeout(function(){
-				$('#fh5co-offcanvas').css('display', 'none');	
-				$('#fh5co-offcanvas').removeClass('animated fadeOutLeft fadeInLeft');
-			}, 1000);
-			$('.js-fh5co-nav-toggle').removeClass('active');
-
-	    	event.preventDefault();
-
-		});
-
-	};
-
-	
-
-	
-
-	// Burger Menu
-	var burgerMenu = function() {
-
-		$('body').on('click', '.js-fh5co-nav-toggle', function(event){
-
-			var $this = $(this);
-
-			$('#fh5co-offcanvas').css('display', 'block');
-			setTimeout(function(){
-				$('#fh5co-offcanvas').addClass('animated fadeInLeft');
-			}, 100);
-			
-			// $('body').toggleClass('fh5co-overflow offcanvas-visible');
-			$this.toggleClass('active');
-			event.preventDefault();
-
-		});
-
-	};
-
-	var scrolledWindow = function() {
-
-		$(window).scroll(function(){
-
-			var header = $('#fh5co-header'),
-				scrlTop = $(this).scrollTop();
-
-
-		   $('#fh5co-home .flexslider .fh5co-overlay').css({
-				'opacity' : (.5)+(scrlTop/2000)
-		   });
-
-		   if ( $('body').hasClass('offcanvas-visible') ) {
-		   	$('body').removeClass('offcanvas-visible');
-		   	$('.js-fh5co-nav-toggle').removeClass('active');
-		   }
-		 
-		});
-
-		$(window).resize(function() {
-			if ( $('body').hasClass('offcanvas-visible') ) {
-		   	$('body').removeClass('offcanvas-visible');
-		   	$('.js-fh5co-nav-toggle').removeClass('active');
-		   }
-		});
-		
-	};
-
-
-	
-
-	// Page Nav
-	var clickMenu = function() {
-		var topVal = ( $(window).width() < 769 ) ? 0 : 58;
-
-		$(window).resize(function(){
-			topVal = ( $(window).width() < 769 ) ? 0 : 58;		
-		});
-
-		if ( $(this).attr('href') != "#") {
-			$('#fh5co-main-nav a:not([class="external"]), #fh5co-offcanvas a:not([class="external"])').click(function(event){
-				var section = $(this).data('nav-section');
-
-
-				if ( $('div[data-section="' + section + '"]').length ) {
-
-					$('html, body').animate({
-			        	scrollTop: $('div[data-section="' + section + '"]').offset().top - topVal
-			    	}, 500);	
-			    	
-			   }
-			   event.preventDefault();
-
-			});
-		}
-
-		
-
-
-	};
-
-	// Loading page
-	var loaderPage = function() {
-		$(".fh5co-loader").fadeOut("slow");
-	};
-
-	// Magnific Popup
-	
-	var magnifPopup = function() {
-		$('.image-popup').magnificPopup({
-			type: 'image',
-			removalDelay: 300,
-			mainClass: 'mfp-with-zoom',
-			titleSrc: 'title',
-			gallery:{
-				enabled:true
-			},
-			zoom: {
-				enabled: true, // By default it's false, so don't forget to enable it
-
-				duration: 300, // duration of the effect, in milliseconds
-				easing: 'ease-in-out', // CSS transition easing function
-
-				// The "opener" function should return the element from which popup will be zoomed in
-				// and to which popup will be scaled down
-				// By defailt it looks for an image tag:
-				opener: function(openerElement) {
-				// openerElement is the element on which popup was initialized, in this case its <a> tag
-				// you don't need to add "opener" option if this code matches your needs, it's defailt one.
-				return openerElement.is('img') ? openerElement : openerElement.find('img');
-				}
-			}
-		});
 	};
 
 	var contentWayPoint = function() {
@@ -181,12 +19,12 @@
 		$('.animate-box').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				
+
 				i++;
 
 				$(this.element).addClass('item-animate');
 				setTimeout(function(){
-					
+
 					$('body .animate-box.item-animate').each(function(k){
 						var el = $(this);
 						setTimeout( function () {
@@ -194,9 +32,9 @@
 							el.removeClass('item-animate');
 						},  k * 200, 'easeInOutExpo' );
 					});
-					
+
 				}, 100);
-				
+
 			}
 
 		} , { offset: '85%' } );
@@ -204,48 +42,11 @@
 
 	};
 
-	var contentWayPointEpic = function() {
-		var i = 0;
-		$('.animate-box-epic').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-					
-					$('body .animate-box-epic.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							el.addClass('fadeIn animated');
-							el.removeClass('item-animate');
-						},  k * 200, 'easeInOutExpo' );
-					});
-					
-				}, 100);
-				
-			}
-
-		} , { offset: '50%' } );
-
-
-	};
-
 	// Document on load.
 	$(function(){
 
-		mobileMenuOutsideClick();
-		burgerMenu();
-		scrolledWindow();
-		
-		loaderPage();
-		magnifPopup();
-
 		// Animations
 		contentWayPoint();
-		contentWayPointEpic();
-		
 
 	});
 
